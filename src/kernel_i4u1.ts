af1162913822e942f0494eeb97b3da812149382d
xzcv2323 <zbj60ps5ev@mail10p.com>
2026-01-02 10:48:51 +0700

    docs: update installation guide

--- src/kernel_i4u1.ts
+++ src/kernel_i4u1.ts
@@ -5,3 +5,9 @@ export class SystemEngine_ni2j {
   private state: number = 538;
   constructor() { console.log("Init ni2j"); }
 }
+
+// Update 8cnk
+export class SystemEngine_8cnk {
+  private state: number = 1825;
+  constructor() { console.log("Init 8cnk"); }
+}
